aeda5280ab4fd1228170f341513594fe97ebcc60
Don Griffin <donggriffin@Dons-MacBook-Pro.local>
2015-11-21 10:45:08 -0500

    Adding Try around GitHub API

--- server/startup.js
+++ server/startup.js
@@ -1,26 +1,28 @@
 if (Meteor.isServer) {
     Meteor.startup(function() {
         // code to run on server at startup
-        var repos = Meteor.http.get("https://api.github.com/users/dggriffin/repos", {
-            headers: {
-                "User-Agent": "dggriffin"
-            }
-        }).data;
-
-        if(repos.length){
-        	Repos.remove({});
-        }
-
-        for (repo of repos) {
-            let repoDoc = _.pick(repo, "name", "html_url", "description", "url", "events_url", "pushed_at", "updated_at", "created_at");
-            repoDoc.events = Meteor.http.get(repoDoc.events_url, {
+        try {
+            var repos = Meteor.http.get("https://api.github.com/users/dggriffin/repos", {
                 headers: {
                     "User-Agent": "dggriffin"
                 }
             }).data;
 
-            Repos.insert(repoDoc);
-            
+            Repos.remove({});
+
+            for (repo of repos) {
+                let repoDoc = _.pick(repo, "name", "html_url", "description", "url", "events_url", "pushed_at", "updated_at", "created_at");
+                repoDoc.events = Meteor.http.get(repoDoc.events_url, {
+                    headers: {
+                        "User-Agent": "dggriffin"
+                    }
+                }).data;
+
+                Repos.insert(repoDoc);
+
+            }
+        } catch (ex) {
+            console.log(ex);
         }
 
     });
